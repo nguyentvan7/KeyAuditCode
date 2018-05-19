343b8a1983f3d03328cb262c0f218f9094d98335
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2018-05-19 02:35:23 -0700

    Add editing of audit
    Program prints key audit in order of keycodes.
    Program now prompts user if information is correct and allows the
    user to edit the audit.

--- KeyAuditReader.py
+++ KeyAuditReader.py
@@ -1,4 +1,4 @@
-from openpyxl import load_workbook
+import openpyxl as xl
 
 # Return amount of rooms based on building. Rows is rooms + 1.
 def building_check(ltr):
@@ -18,7 +18,7 @@ def building_check(ltr):
         return 149
 
 # Load full key audit.
-auditBook = load_workbook(filename = "VDSKeyAudit.xlsx")
+auditBook = xl.load_workbook(filename = "VDSKeyAudit.xlsx")
 auditSheetB = auditBook['B']
 auditSheetC = auditBook['C']
 auditSheetD = auditBook['D']
@@ -30,12 +30,56 @@ auditSheetI = auditBook['I']
 auditSheetJ = auditBook['J']
 auditSheetK = auditBook['K']
 
+# Load sorted keycodes (1-1866).
+# Rooms are stored in column A, keycodes are stored in column B.
+sortedBook = xl.load_workbook(filename = "SortedDecoded.xlsx")
+sortedSheet = sortedBook["Vista"]
+
 # Setup for reading audit.
 columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-roomAmount = building_check('B')
 
 # Reading audit back to user.
-for row in range(2, roomAmount):
-    for column in columns:
-        print(auditSheetB[column + "1"].value + ": " + str(auditSheetB[column + str(row)].value))
+for currentKey in range(1, 1867):
+    # Decode building from currentKey.
+    currentRoom = sortedSheet["A" + str(currentKey + 1)].value
+    currentLetter = currentRoom[3]
+    roomAmount = building_check(currentLetter)
+    auditSheet = auditBook[currentLetter]
+    print("Keycode: " + str(currentKey))
+    for row in range(2, roomAmount):
+        for column in columns:
+            if currentRoom[5:] == auditSheet["A" + str(row)].value:
+                print(auditSheet[column + "1"].value + ": " + str(auditSheet[column + str(row)].value))
+                selectedRow = row
+    verify = input("Do any changes need to be made?\n")
+    # Changes need to be made.
+    if verify != "":
+        choice = input("What field needs to be changed?\n")
+        while choice != "done":
+            # Changing sparky key amount.
+            if choice == "s":
+                auditSheet["D" + str(selectedRow)].value = input("How many sparky keys?\n")
+            # Changing room key amount.
+            elif choice == "r":
+                auditSheet["E" + str(selectedRow)].value = input("How many room keys?\n")
+            # Changing mail key amount.
+            elif choice == "m":
+                auditSheet["F" + str(selectedRow)].value = input("How many mail keys?\n")
+            # Changing fob amount.
+            elif choice == "f":
+                auditSheet["G" + str(selectedRow)].value = input("How many fobs?\n")
+            # Changing clear/discrepant.
+            elif choice == "c":
+                change = input("Clear or discrepant?\n")
+                if change == "c":
+                    auditSheet["H" + str(selectedRow)].value = "Clear"
+                else:
+                    auditSheet["H" + str(selectedRow)].value = "Discrepant"
+            # Changing comments.
+            else:
+                auditSheet["I" + str(selectedRow)].value = input("Input comment:\n")
+            choice = input("What field needs to be changed?\n")
+        auditBook.save("VDSKeyAudit.xlsx")
+        break
     print()
+#auditBook.save("VDSKeyAudit.xlsx")
